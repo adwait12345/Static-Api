06b522df3aa5516955f674df7873c75055c02cb3
adwait12345 <advtbokde@gmail.com>
2023-10-17 16:15:44 +0530

    more data

---
+++ data/performanceData.js
@@ -0,0 +1,49 @@
+const PerformanceData = [
+    {
+        name: "Rolley X",
+        leads: 45,
+        sales: "1,000,000",
+        quota: "1,200,000",
+        variance: "low",
+        region: "Region A",
+        status: "overperforming",
+    },
+    {
+        name: "Rolley Xs",
+        leads: 35,
+        sales: "900,000",
+        quota: "1,000,000",
+        variance: "low",
+        region: "Region B",
+        status: "average",
+    },
+    {
+        name: "Rolley Mt",
+        leads: 52,
+        sales: "930,000",
+        quota: "1,000,000",
+        variance: "medium",
+        region: "Region C",
+        status: "underperforming",
+    },
+    {
+        name: "Rolley Nx",
+        leads: 22,
+        sales: "390,000",
+        quota: "250,000",
+        variance: "low",
+        region: "Region A",
+        status: "overperforming",
+    },
+    {
+        name: "Rolley Qx",
+        leads: 49,
+        sales: "860,000",
+        quota: "750,000",
+        variance: "low",
+        region: "Region B",
+        status: "overperforming",
+    },
+]
+
+module.exports= PerformanceData;
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(cors())
 const Data = require('./data/vehicle')
 const Performance = require('./data/performance')
 const Kpidata = require('./data/kpiData')
+const PerformanceData = require('./data/performanceData')
 
 app.get('/live-vehicles', (req, res) => {
     res.json({ Data });
@@ -18,6 +19,10 @@ app.get('/performance', (req, res) => {
 
 app.get('/kpi-data', (req, res) => {
     res.json({ Kpidata});
+});
+
+app.get('/performance-data', (req, res) => {
+    res.json({ PerformanceData });
 });
 
 app.listen(8000, () => {
